6556a1a244cc156c9e737c0db8ce1d82d463cd66
Brenda Wang <u6374399@anu.edu.au>
2017-11-24 13:51:47 +1100

    Ported new duplicated MMTk over

---
+++ tools/bootloader/sysMMTk.c
@@ -0,0 +1,13 @@
+#include "sys.h"
+
+#include <stdlib.h> // malloc and others
+#include <errno.h> // error numbers
+#include <string.h> // memcpy & memmove
+#include <sys/mman.h> // mmap
+
+EXTERNAL void sysMemmove(void *dst, const void *src, Extent cnt)
+{
+  printf("Test2\n");
+  TRACE_PRINTF("%s: sysMemmove %p %p %zu\n", Me, dst, src, cnt);
+  memmove(dst, src, cnt);
+}
