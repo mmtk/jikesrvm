0146e8ccf024f5f1827514197058e1c04082d44b
Brenda Wang <u6374399@anu.edu.au>
2017-12-12 19:59:10 +1100

    Fixed scripts so it exits correctly
    Former-commit-id: 5ab3eda10478f829ed6d8832f73604bfa157a267

--- scripts/testBaseBase.py
+++ scripts/testBaseBase.py
@@ -12,8 +12,6 @@ build = 0
 passes = 0
 tests = 0
 
-# this doesnt work :(
-
 for x in range(0, 5):
     build = 0
     for y in range(0, 3):
@@ -22,10 +20,12 @@ for x in range(0, 5):
         build += 1
     if build == 3:
         print ("build failure :(")
-        break
+        exit(1)
     tests += 1
     if exe("dist/RBaseBaseNoGC_x86_64-linux/rvm -Xms500M -jar benchmarks/dacapo-2006-10-MR2.jar fop".split()) != 0:
         break
     passes += 1
 
 print ("Score: " + str(passes) + "/" + str(tests))
+if passes != test:
+    exit(1)
--- scripts/testFastAdaptive.py
+++ scripts/testFastAdaptive.py
@@ -1,6 +1,5 @@
 import subprocess
 
-
 def exe(cmd, env=None):
     print "{}".format(cmd)
     p = subprocess.Popen(cmd,
@@ -12,8 +11,6 @@ build = 0
 passes = 0
 tests = 0
 
-# this doesnt work :(
-
 build = False
 for y in range(0, 2):
     if exe("bin/buildit localhost -j /usr/lib/jvm/default-java RBaseBaseNoGC --nuke".split()) == 0:
@@ -21,7 +18,9 @@ for y in range(0, 2):
 			break
 if build == False:
     print ("build failure :(")
+    exit(1)
 elif exe("dist/RBaseBaseNoGC_x86_64-linux/rvm -Xms500M -jar benchmarks/dacapo-2006-10-MR2.jar fop".split()) != 0:
     print ("Passed FastAdaptive")
 else:
     print ("Failed FastAdaptive") 
+    exit(1)
