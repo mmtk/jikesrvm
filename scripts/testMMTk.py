73fcc716c01e41eede61e46019ea7f68fb934533
Brenda Wang <u6374399@anu.edu.au>
2018-02-06 09:05:08 +1100

    Added build testing to script
    Former-commit-id: 2c1581c5135fb7868bfd5140cb32e59018b6510b

--- scripts/testMMTk.py
+++ scripts/testMMTk.py
@@ -9,12 +9,12 @@ os.chdir(a)         # change to base directory
 
 # Arguments to be passed
 parser = argparse.ArgumentParser(description="Runs tests to verify it compiles and builds")
-parser.add_argument("-g", dest="collector", default="BaseBaseNoGC", help="Specifies which garbage collector to use", type=str)
+parser.add_argument("-g", dest="collector", default="", help="Specifies which garbage collector to use", type=str)
 parser.add_argument("-n", default=1, dest="tests", help="Specifies the number of times to recompile and run the file", type=int)
-parser.add_argument("-a", dest="args", default="-Xms1024M", help="Specifies which arguments to pass when testing MMTk", type=str)
+parser.add_argument("-T", dest="test_run", default="", help="Specifies what group of configured tests to run when building", type=str)
+parser.add_argument("-a", dest="args", default="Xms1024M", help="Specifies which arguments to pass when testing MMTk", type=str)
 parser.add_argument("--build-only", dest="build_only", action="store_true", help="Only build the compiler but do not test it")
 parser.add_argument("--test-only", dest="test_only",action="store_true", help="Do not build the compiler, only test it")
-parser.add_argument("--java-build", dest="java_build", action="store_true", help="Build using the java MMTk instead of the rust MMTk")
 
 args = parser.parse_args()
 
@@ -37,7 +37,7 @@ def exe(cmd, env=None):
     return p.returncode
 
 # Generates a list of all garbage collectors supported by MMTk
-garbage_collector_list = []
+garbage_collector_list = [""]
 for file in glob.glob(os.path.join("build", "configs", "*.properties")):
     name = os.path.basename(file)
     fname,lname = name.split(".")
@@ -56,12 +56,12 @@ if args.build_only and args.test_only:
     print ("Must either build or test compiler")
     exit(1)
 
-# Set the arguments depending on which MMTk
-# version is being built
-if args.java_build:
-    args.java_build = ""
-else:
-    args.java_build = "R"
+if args.test_run != "":
+	args.test_run = "--test-run " + args.test_run
+
+args.args = args.args.replace(" ", " -")
+args.args = "-" + args.args
+print (args.args)
 
 passes = 0
 
@@ -75,7 +75,7 @@ for _ in range(0, args.tests):
         # All other build errors return errors as usual
         for _ in range(0, 3):
             rc = exe(("bin/buildit localhost -j /usr/lib/jvm/default-java --answer-yes " +
-                      args.java_build + args.collector + " --nuke").split())
+                      args.collector + args.test_run + " --nuke").split())
             if rc != 3:
                 build = rc == 0
                 break
@@ -84,7 +84,7 @@ for _ in range(0, args.tests):
             exit(1)
     # Test if not build only
     if not args.build_only:
-        if exe(("dist/" + args.java_build + args.collector + "_x86_64-linux/rvm "
+        if exe(("dist/" + args.collector + "_x86_64-linux/rvm "
                     + args.args + " -jar benchmarks/dacapo-2006-10-MR2.jar fop").split()) != 0:
             if passes != 0:
                 print ("Test failed. Succeeded previously " + str(passes) + " times.")
