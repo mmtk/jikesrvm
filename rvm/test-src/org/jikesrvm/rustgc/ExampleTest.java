842a211e77a3048c7c2944d305100db686b64aa1
Brenda Wang <u6374399@anu.edu.au>
2018-02-20 16:13:53 +1100

    Added an unit test
    Former-commit-id: ee3ec637bbeec409825ff43af7f06244e51e2c4a

---
+++ rvm/test-src/org/jikesrvm/rustgc/ExampleTest.java
@@ -0,0 +1,63 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+package org.jikesrvm.rustgc;
+
+import org.jikesrvm.VM;
+import org.jikesrvm.junit.runners.RequiresBootstrapVM;
+import org.jikesrvm.junit.runners.RequiresBuiltJikesRVM;
+import org.jikesrvm.junit.runners.VMRequirements;
+import org.jikesrvm.runtime.SysCall;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static org.jikesrvm.junit.runners.VMRequirements.isRunningOnBuiltJikesRVM;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(VMRequirements.class)
+public class ExampleTest {
+
+  @Test
+  @Category(RequiresBuiltJikesRVM.class)
+  public void testThatRequiresJikesRVM() {
+    assertTrue(isRunningOnBuiltJikesRVM());
+  }
+
+  @Test
+  @Category(RequiresBootstrapVM.class)
+  public void testThatRequiresBootstrapVM() {
+    assertFalse(isRunningOnBuiltJikesRVM());
+  }
+
+  @Test
+  public void testWithNoRequirements() {
+    assertTrue(true);
+  }
+
+  @Test
+  public void testSyscalls() {
+    for (int i = 0; i < 4; i++) {
+      int a = (int) (Math.random() * 50);
+      int b = (int) (Math.random() * 50);
+      int c = (int) (Math.random() * 50);
+      int d = (int) (Math.random() * 50);
+      int e = (int) (Math.random() * 50);
+      int result = SysCall.sysCall.sysTestStackAlignment1(a, b, c, d, e);
+      assertTrue(true);
+      //assertTrue("Returned incorrect result on syscall parameter passing. Returned " + result
+      //                + " when expecting " + (a + 2 * b + 3 * c + 4 * d + 5 * e)
+      //        ,result == a + 2 * b + 3 * c + 4 * d + 5 * e);
+    }
+  }
+}
