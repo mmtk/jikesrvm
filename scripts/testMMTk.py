dc937b8f2ecc0f5c7a5873e75527cc2a05e626fe
Brenda Wang <u6374399@anu.edu.au>
2018-02-23 11:48:48 +1100

    Refactored script
    Former-commit-id: 1cc010db207014effc444accbbffedfda5407cb4

--- scripts/testMMTk.py
+++ scripts/testMMTk.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python
-import subprocess   # run program
-import argparse     # parsing arguments to python
-import sys          # run program
-import glob, os     # retrieve files in directory
+import subprocess  # run program
+import argparse  # parsing arguments to python
+import sys  # run program
+import glob, os  # retrieve files in directory
 
-a = os.path.abspath(os.path.join(__file__, "..", ".." ))
-os.chdir(a)         # change to base directory
+a = os.path.abspath(os.path.join(__file__, "..", ".."))
+os.chdir(a)  # change to base directory
 
 # Arguments to be passed
 parser = argparse.ArgumentParser(description="Runs tests to verify it compiles and builds")
 parser.add_argument("-g", dest="collector", default="", help="Specifies which garbage collector to use", type=str)
-parser.add_argument("-n", default=1, dest="tests", help="Specifies the number of times to recompile and run the file", type=int)
-parser.add_argument("-T", dest="test_run", default="", help="Specifies what group of configured tests to run. Do not specify a garbage collector with this", type=str)
-parser.add_argument("-t", dest="test", default="", help="Specifies what specific test to run when building. This one requires a specific garbage collector to run the tests on", type=str)
-parser.add_argument("-a", dest="args", default="Xms1024M", help="Specifies which arguments to pass when testing MMTk", type=str)
-parser.add_argument("--build-only", dest="build_only", action="store_true", help="Only build the compiler but do not test it")
+parser.add_argument("-n", default=1, dest="tests", help="Specifies the number of times to recompile and run the file",
+                    type=int)
+parser.add_argument("-T", dest="test_run", default="",
+                    help="Specifies what group of configured tests to run. Do not specify a garbage collector with this",
+                    type=str)
+parser.add_argument("-t", dest="test", default="",
+                    help="Specifies what specific test to run when building. This one requires a specific garbage collector to run the tests on",
+                    type=str)
+parser.add_argument("-a", dest="args", default="Xms1024M", help="Specifies which arguments to pass when testing MMTk",
+                    type=str)
+parser.add_argument("--build-only", dest="build_only", action="store_true",
+                    help="Only build the compiler but do not test it")
 parser.add_argument("-b", dest="build_args", help="Specifies which arguments to pass when building JikesRVM", type=str)
-parser.add_argument("--test-only", dest="test_only",action="store_true", help="Do not build the compiler, only test it")
+parser.add_argument("--test-only", dest="test_only", action="store_true",
+                    help="Do not build the compiler, only test it")
 
 args = parser.parse_args()
+
 
 def exe(cmd, env=None):
     print ("{}".format(cmd))
@@ -38,6 +47,7 @@ def exe(cmd, env=None):
     p.wait()
     return p.returncode
 
+
 # Generates a list of all garbage collectors supported by MMTk
 if args.test_run != "":
     garbage_collector_list = [""]
@@ -45,10 +55,10 @@ else:
     garbage_collector_list = []
 for file in glob.glob(os.path.join("build", "configs", "*.properties")):
     name = os.path.basename(file)
-    fname,lname = name.split(".")
+    fname, lname = name.split(".")
     garbage_collector_list.append(fname)
 
-b = os.path.abspath(os.path.join(__file__, "..", ".." ))  # files in directory
+b = os.path.abspath(os.path.join(__file__, "..", ".."))  # files in directory
 os.chdir(b)
 
 # Check arguments passed are correct
@@ -62,11 +72,10 @@ if args.build_only and args.test_only:
     exit(1)
 
 if args.test_run != "":
-	args.test_run = "--test-run " + args.test_run
+    args.test_run = "--test-run " + args.test_run
 
 if args.test != "":
-	args.test = " -t " + args.test
-
+    args.test = " -t " + args.test
 
 args.args = args.args.replace(" ", " -")
 args.args = "-" + args.args
@@ -95,7 +104,7 @@ for _ in range(0, args.tests):
             print ("Build failed.")
             exit(1)
     # Test if not build only
-    if not args.build_only and args.collector!="":
+    if not args.build_only and args.collector != "":
         if exe(("dist/" + args.collector + "_x86_64-linux/rvm "
                     + args.args + " -jar benchmarks/dacapo-2006-10-MR2.jar fop").split()) != 0:
             if passes != 0:
@@ -106,7 +115,7 @@ for _ in range(0, args.tests):
                 exit(12)
             exit(1)
         passes += 1  # Check if the script has passed
-if args.collector=="":
+if args.collector == "":
     print ("Tests performed.")
 elif passes == args.tests:
     print ("Tests passed.")
