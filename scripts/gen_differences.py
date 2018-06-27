a235d6772b73fcb200efdb70ba2965ffa5a4a59d
Brenda Wang <u6374399@anu.edu.au>
2018-06-27 20:10:48 +1000

    Cleaned up testing scripts
    Former-commit-id: d57a5156238c4d7ac76b5c8cdfdef5ccddcaf564

--- scripts/gen_differences.py
+++ scripts/gen_differences.py
@@ -1,38 +1,88 @@
+#!/usr/bin/env python
 import json
 import logging, sys
 import glob
 from pathlib import Path
-
+import os
 
 # Set logging level. Levels are debug, info, warning, error, critical
-logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
-# Get all json files that are appended with R
-rust_files = glob.glob("results/R*.json")
-logging.debug(("All rust files found: {}").format(rust_files))
+##################
+# Base arguments #
+##################
 
-for rust_file in rust_files:
-    logging.info(("Processing {}").format(rust_file))
-    # Check if there are any equivalent Java MMTk json files
-    java_file = Path(rust_file.replace("results/R","results/"))
-    if not java_file.is_file():
-        logging.warn("File not found")
+abs_dir   = Path(os.path.abspath(os.path.dirname(__file__)))
+json_dir  = abs_dir / ".." / "results" / "json"
+
+################################
+# Get all build configurations #
+################################
+
+build_dirs = os.listdir(json_dir)
+logging.info(("Build configurations available are: {}").format(build_dirs))
+
+###########################################
+# Specify the json directories to compare #
+###########################################
+
+while True:
+    first_build = input("Enter a build configuration: ")
+    first_build.replace("/", "")
+    if first_build not in build_dirs:
+        print("Incorrect build configuration")
+    else:
+        logging.info("First config set to: {}".format(first_build))
+        break
+
+# Note should we just make this append R cos im lazy
+while True:
+    second_build = input("Enter a build configuration: ")
+    second_build.replace("/", "")
+    if second_build not in build_dirs or first_build == second_build:
+        print("Incorrect build configuration")
+    else:
+        logging.info("Second config set to: {}".format(second_build))
+        break
+
+##############################################
+# Retrieve files associated with each config #
+##############################################
+
+first_dir  = json_dir / first_build
+second_dir = json_dir / second_build
+
+first_files = list(first_dir.glob("*.json"))
+second_files = list(second_dir.glob("*.json"))
+logging.debug("First configuration files found: {}".format(first_files))
+logging.debug("Second configuration files found: {}".format(second_files))
+
+####################################
+# Compare both configuration files #
+####################################
+
+logging.debug("Intersection: {}".format(list(set(first_files) & set(second_files))))
+logging.debug("Union: {}".format(list(set(first_files) | set(second_files))))
+
+for first_file in first_files:
+    logging.debug(("{}: Processing from {}").format(first_build, first_file))
+    # Get equivalent second file
+    second_file = Path(str(first_file).replace(first_build, second_build))
+    if not second_file.is_file():
+        logging.info(("File {} not found").format(second_file))
         continue
     else:
-        logging.info(("File at {} is found").format(java_file))
-        # Open rust and java files to compare them
-        with open(java_file) as jf, open(rust_file) as rf:
-            logging.debug("Successfully opened java and rust file")
-            java_data = json.load(jf)
-            rust_data = json.load(rf)
-            for (jtest,jres), (rtest,rres) in zip(java_data.items(), rust_data.items()):
-                logging.debug(("jtest:{}, jres:{}, rtest:{}, rres:{}").format(jtest, jres, rtest, rres))
-                # These tests should be the same
-                assert jtest == rtest, "Tests are not in correct order / missing. Most likely something is wrong with parser or testing framework"
-                if (rres["exit-code"] != jres["exit-code"]):
-                    print("=================",jtest,"=================")
-                    print("------ Java returned exit code", jres["exit-code"],"------")
-                    print(jres["output"])
-                    print("------ Rust returned exit code", rres["exit-code"],"------")
-                    print(rres["output"])
+        logging.info(("File {} found").format(second_file))
+        with open(first_file) as f1, open(second_file) as f2:
+            d1 = json.load(f1)
+            d2 = json.load(f2)
+            for (t1, r1), (t2, r2) in zip(d1.items(), d2.items()):
+                logging.debug(("test1:{}, res1:{}, test2:{}, res2:{}").format(t1, r1, t2, r2))
+                assert t1 == t2
+                if (r2["exit-code"] != r1["exit-code"]):
+                    print("=================",t1,"=================")
+                    print("------ {} succeeded: {}, returned exit code {} ------".format(first_build, r1["result"], r1["exit-code"]))
+                    print(r1["output"])
+                    print("------ {} succeeded: {}, returned exit code {} ------".format(first_build, r2["result"], r2["exit-code"]))
+                    print(r2["output"])
                     
--- scripts/parse_results.py
+++ scripts/parse_results.py
@@ -1,21 +1,72 @@
+#!/usr/bin/env python
 from bs4 import BeautifulSoup
 import json
+from pathlib import Path
+import pathlib
+import os
 
-with open("results/buildit/latest/tests/local/Results.xml") as xml:
+##################
+# Base arguments #
+##################
+
+abs_dir   = Path(os.path.abspath(os.path.dirname(__file__)))
+base_dir  = abs_dir / ".." / "results"
+json_dir  = base_dir / "json"
+build_dir = base_dir / "buildit"
+xml_dir   = Path("tests") / "local" / "Results.xml"
+
+######################################
+# Specify results directory to parse #
+######################################
+
+# Specify a directory in the results folder to look at
+result_dir = input("Enter a directory to look at: ")
+# Sanitise '/' characters
+result_dir.replace("/", "")
+# If none is specified, look at the latest
+if result_dir == "":
+    result_dir = "latest"
+
+#############################
+# Open Results.xml to parse #
+#############################
+
+# Open Results.xml and get it ready for parsing
+with open(build_dir / result_dir / xml_dir) as xml:
     results = BeautifulSoup(xml, "xml")
     root = results.results.find("test-configuration")
 
+############################
+# Find build configuration #
+############################
 
-
-# Print out which configuration is being tested
+# Find the configuration which can be tested
 build_config = root.find("build-configuration").contents[0]
 
+########################
+# Find each test group #
+########################
+
+# For each group, create a dictionary of the results and dump it in a json file
 for group in root.find_all("group"):
-    file_name = "results/"+build_config+"-"+group.find("name").contents[0]+".json"
+
+    ################################
+    # Create dictionary of results #
+    ################################
+
+    # Make the dictionary with all results
     result = {}
     for test in group.find_all("test"):
         result[test.find("name").contents[0]] = {"result": test.find("test-execution").result.contents[0]=="SUCCESS",
                                                   "exit-code": test.find("test-execution").find("exit-code").contents[0],
                                                   "output": test.find("test-execution").find("output").contents[0]}
+
+    ###################################
+    # Create and dump results in json #
+    ###################################
+
+    file_dir = json_dir / build_config
+    file_dir.mkdir(parents=True, exist_ok=True) 
+    file_name = file_dir / (group.find("name").contents[0] + ".json")
     with open(file_name, 'w') as fp:
         json.dump(result, fp)
