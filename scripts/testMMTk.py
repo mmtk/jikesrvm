b8a2796238ad9012c2085355255dc19cb67006f0
Brenda Wang <u6374399@anu.edu.au>
2018-07-02 16:56:13 +1000

    Fixed python import issues for testMMTk script
    Former-commit-id: 5195fc3364aaf08d2ceb31c07affc81c1e0bb00a

--- scripts/testMMTk.py
+++ scripts/testMMTk.py
@@ -3,20 +3,12 @@ import subprocess  # run program
 import argparse  # parsing arguments to python
 import sys  # run program
 import glob, os  # retrieve files in directory
-from pathlib import Path
-import logging
-import helper
 
-jikesrvm_dir = Path(os.path.abspath(os.path.dirname(__file__))) / ".."
+a = os.path.abspath(os.path.join(__file__, "..", ".."))
+os.chdir(a)  # change to base directory
 
-# Set logging level. Levels are debug, info, warning, error, critical
-logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
-
-######################
-# Arguments to parse #
-######################
-
-parser = argparse.ArgumentParser(description="Runs tests to verify whether it compiles and builds")
+# Arguments to be passed
+parser = argparse.ArgumentParser(description="Runs tests to verify it compiles and builds")
 parser.add_argument("-g", dest="collector", default="", help="Specifies which garbage collector to use", type=str)
 parser.add_argument("-n", default=1, dest="tests", help="Specifies the number of times to recompile and run the file",
                     type=int)
@@ -26,7 +18,7 @@ parser.add_argument("-T", dest="test_run", default="",
 parser.add_argument("-t", dest="test", default="",
                     help="Specifies what specific test to run when building. This one requires a specific garbage collector to run the tests on",
                     type=str)
-parser.add_argument("-j", dest="java_home", default="/usr/lib/jvm/default-java", 
+parser.add_argument("-j", dest="java_home", default="/usr/lib/jvm/default-java",
                     help="Specifies the location of JAVA_HOME", type=str)
 parser.add_argument("-a", dest="args", default="Xms1024M", help="Specifies which arguments to pass when testing MMTk",
                     type=str)
@@ -34,15 +26,45 @@ parser.add_argument("--build-only", dest="build_only", action="store_true",
                     help="Only build the compiler but do not test it")
 parser.add_argument("-b", dest="build_args", default="", help="Specifies which arguments to pass when building JikesRVM", type=str)
 parser.add_argument("--test-only", dest="test_only", action="store_true",
-                    help="Do not build the compiler, only test it"),
-parser.add_argument("--logging", dest="log", help="Specifies the verbosity of logging", type=str)
+                    help="Do not build the compiler, only test it")
+
 args = parser.parse_args()
+
+
+def exe(cmd, env=None):
+    print ("{}".format(cmd))
+    p = subprocess.Popen(cmd,
+                         env=env,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    stdout = []
+    while True:
+        line = p.stdout.readline()
+        stdout.append(line)
+        sys.stdout.write(line)
+        if ("corrupted zip file") in line:
+            return 3
+        if line == '' and p.poll() != None:
+            break
+    p.wait()
+    return p.returncode
 
 ########################################
 # Generate list of all valid GC schemes#
 ########################################
 
-garbage_collector_list = helper.get_builds(args, jikesrvm_dir)
+# Generates a list of all garbage collectors supported by MMTk
+if args.test_run != "":
+    garbage_collector_list = [""]
+else:
+    garbage_collector_list = []
+for file in glob.glob(os.path.join("build", "configs", "*.properties")):
+    name = os.path.basename(file)
+    fname, lname = name.split(".")
+    garbage_collector_list.append(fname)
+
+b = os.path.abspath(os.path.join(__file__, "..", ".."))  # files in directory
+os.chdir(b)
 
 ########################################
 # Check arguments are passed correctly #
@@ -78,33 +100,38 @@ if args.build_args != "":
 #############
 
 build = False
-test_passed = 0
+passes = 0
+
+# Run tests
 for _ in range(0, args.tests):
+    # Build if not test only
     if not args.test_only:
-        #################
-        # Build the jvm #
-        #################
-        build = helper.build_jvm(args)
+        # Corrupted zips return an error code of 3.
+        # If it corrupts, rerun at most 3 times
+        # All other build errors return errors as usual
+        for _ in range(0, 3):
+            rc = exe(("bin/buildit localhost -j " + args.java_home + " --answer-yes " +
+                      args.build_args + args.collector + args.test + args.test_run + " --nuke").split())
+            if rc != 3:
+                build = rc == 0
+                break
         if not build:
             print ("Build failed.")
             exit(1)
+    # Test if not build only
     if not args.build_only and args.collector != "":
-        ##################
-        # Test the build #
-        ##################
-        test = helper.test_jvm(args)
-        if test != 0:
-            if test_passed != 0:
-                print("Test failed. Succeeded previously " + str(test_passed) + " times.")
+        if exe(("dist/" + args.collector + "_x86_64-linux/rvm "
+                    + args.args + " -jar benchmarks/dacapo-2006-10-MR2.jar fop").split()) != 0:
+            if passes != 0:
+                print ("Test failed. Succeeded previously " + str(passes) + " times.")
+                exit(12)
             else:
-                print("Test failed.")
+                print ("Test failed.")
             exit(1)
-        test_passed += 1
-
-
+        passes += 1  # Check if the script has passed
 if args.collector == "":
     print ("Tests performed.")
-elif test_passed == args.tests:
+elif passes == args.tests:
     print ("Tests passed.")
 elif build:
     print ("Build successful")
