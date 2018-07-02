7ead0f3bcb6eae75a7aa97cf087cf15a20d6330d
Brenda Wang <u6374399@anu.edu.au>
2018-07-02 14:47:48 +1000

    Cleaned up testMMTk code and refactored some to helper
    Former-commit-id: 117af8b259873d929855f724f373157f980baeed

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,5 +1,8 @@
 import logging
 import math
+from pathlib import Path, PurePath
+import subprocess
+import sys
 
 def parse_input(valid_inputs, input_message="Enter a string: ", output_message="Input not valid",
                 sanitise=True, null_value="", invalid_inputs = []):
@@ -38,3 +41,77 @@ def get_header(test_name, result, build):
 def append_equals(text, length):
     equals_length = length - len(text)
     return "=" * math.floor(equals_length / 2) + text + "=" * math.ceil(equals_length / 2)
+
+def exe(cmd, env=None, check_corrupted=False):
+    print ("{}".format(cmd))
+    p = subprocess.Popen(cmd,
+                         env=env,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    stdout = []
+    if check_corrupted:
+        while True:
+            line = p.stdout.readline().decode("utf-8")
+            stdout.append(line)
+            sys.stdout.write(line)
+            if ("corrupted zip file") in line:
+                return 3
+            if line == '' and p.poll() != None:
+                break
+    p.wait()
+    return p.returncode
+
+def build_jvm(args, rerun_corrupted=3):
+    """
+    Builds JVM with particular collector
+
+    Args:
+        args: List of build arguments
+        rerun_corrupted: Number of times to rerun if corrupted zip is found
+    Returns:
+        Whether or not the JVM built successfully or not
+    """
+    assert rerun_corrupted >= 0, "Number of times to rerun corrupted must be greater than 0"
+    build = False
+    for _ in range(0, rerun_corrupted):
+        cmd = ("bin/buildit localhost -j " + args.java_home + " --answer-yes " + 
+            args.build_args + args.collector + args.test + args.test_run + " --nuke").split()
+        exit_code = exe(cmd, check_corrupted=True)
+        if exit_code != 3:
+            build = exit_code == 0
+            break
+    return build
+
+def test_jvm(args):
+    """
+    Tests the particular build configuration
+
+    Args:
+        args: List of test arguments
+    Returns:
+        The exit-code of the test
+    """
+    cmd = ("dist/" + args.collector + "_x86_64-linux/rvm "
+                   + args.args + " -jar benchmarks/dacapo-2006-10-MR2.jar fop").split()
+    return exe(cmd)
+
+def get_builds(args, jikesrvm_dir, path=Path("build")/"configs"):
+    """
+    Gets all garbage collection schemes
+
+    Args:
+        jikesrvm_dir: The root directory of JikesRVM
+        path: The path to look at for build configurations
+    Returns:
+        The exit-code of the test
+    """
+    if args.test_run != "":
+        garbage_collector_list = [""]
+    else:
+        garbage_collector_list = []
+    conf = jikesrvm_dir / path
+    for file in conf.glob("*.properties"):
+        name = file.name
+        build_conf, mach_conf = name.split(".")
+        garbage_collector_list.append(build_conf)
+    return garbage_collector_list
