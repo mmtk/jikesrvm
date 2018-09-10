f00265b08922e0d2dbcaf638e25411003ab57d94
Brenda Wang <u6374399@anu.edu.au>
2018-09-10 15:46:38 +1000

    Added argument passing to gen_differences
    Former-commit-id: 523188cddd25c8833782fa470d0f6d408563788e

--- scripts/gen_differences.py
+++ scripts/gen_differences.py
@@ -5,6 +5,8 @@ import glob
 from pathlib import Path
 import os
 import helper
+import argparse
+import shutil
 
 # Set logging level. Levels are debug, info, warning, error, critical
 logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
@@ -18,6 +20,15 @@ config_file = abs_dir / "config.txt"
 results_dir = abs_dir / ".." / "results" 
 json_dir  = results_dir / "json"
 
+#############
+# Arguments #
+#############
+
+parser = argparse.ArgumentParser(description="Generate differences of two files")
+parser.add_argument("--g1", dest="c1", default="", help="Specifies the first garbage collector to use", type=str)
+parser.add_argument("--g2", dest="c2", default="", help="Specifies the second garbage collector to use", type=str)
+args = parser.parse_args()
+
 ################################
 # Get all build configurations #
 ################################
@@ -29,11 +40,15 @@ logging.info(("Build configurations available are: {}").format(build_dirs))
 # Specify the json directories to compare #
 ###########################################
 
-first_build = helper.parse_input(build_dirs, input_message="Enter a build configuration: ",
-                output_message="Incorrect build configuration")
-second_build = helper.parse_input(build_dirs, input_message="Enter a build configuration: ",
-                output_message="Incorrect build configuration", invalid_inputs=[first_build],
-                null_value=("R" + first_build))
+if args.c1 != "" and args.c2 != "":
+    first_build = args.c1
+    second_build = args.c2
+else:
+    first_build = helper.parse_input(build_dirs, input_message="Enter a build configuration: ",
+                    output_message="Incorrect build configuration")
+    second_build = helper.parse_input(build_dirs, input_message="Enter a build configuration: ",
+                    output_message="Incorrect build configuration", invalid_inputs=[first_build],
+                    null_value=("R" + first_build))
 
 ################################
 # Order configs alphabetically #
@@ -63,7 +78,8 @@ logging.debug("Union: {}".format(list(set(first_files) | set(second_files))))
 ############################
 
 diff_dir = results_dir / "diff" / (first_build + "_" + second_build)
-diff_dir.mkdir(parents=True, exist_ok=True) 
+shutil.rmtree(diff_dir)
+diff_dir.mkdir(parents=True, exist_ok=False) 
 
 ################################
 # Load unique cases for builds #
@@ -92,12 +108,16 @@ for first_file in first_files:
         output_file  = str(diff_dir / (os.path.splitext(os.path.basename(str(first_file)))[0]))
         output_file1 = output_file + "-" + first_build + ".txt"
         output_file2 = output_file + "-" + second_build + ".txt"
-        with open(first_file) as f1, open(second_file) as f2, open(output_file1, 'w+') as o1, open(output_file2, 'w+') as o2:
-            
+
+        results1 = {}
+        results2 = {}
+
+        with open(first_file) as f1, open(second_file) as f2:
+
             ################################
             # Compare each individual test #
             ################################
-        
+
             d1 = json.load(f1)
             d2 = json.load(f2)
             for (t1, r1), (t2, r2) in zip(d1.items(), d2.items()):
@@ -108,6 +128,22 @@ for first_file in first_files:
                 if (r2["result"] == special.get(second_build, {}).get(t2, {}).get("result")) and (r2["exit-code"] == str(special.get(second_build, {}).get(t2, {}).get("exit-code"))):
                     continue
                 if (r2["result"] != r1["result"] or r2["exit-code"] != r1["exit-code"]):
-                    helper.write_result(o1, t1, r1, first_build)
-                    helper.write_result(o2, t2, r2, second_build)
-                    
+                    results1[t1] = r1
+                    results2[t2] = r2                    
+
+        assert len(results1) == len(results2)
+
+        if len(results1) > 0:
+
+            with open(output_file1, "w+") as o1, open(output_file2, 'w+') as o2:
+                
+                for (t1, r1), (t2, r2) in zip(results1.items(), results2.items()):
+                    name_output1, info_output1 = helper.get_header(t1, r1, first_build)
+                    name_output2, info_output2 = helper.get_header(t2, r2, second_build)
+                    o1.write(name_output1 + "\n")
+                    o1.write(info_output1 + "\n\n")
+                    o1.write(r1["output"] + "\n")
+                    o2.write(name_output2 + "\n")
+                    o2.write(info_output2 + "\n\n")
+                    o2.write(r2["output"] + "\n")
+
