b98a71b880026ea3bcfc011c7f6524fe958fb5dd
Brenda Wang <u6374399@anu.edu.au>
2018-09-21 14:46:03 +1000

    Refactored some code to helper
    Former-commit-id: 6c1ca2d213644b910cc0e183de8a7290341a9808

--- scripts/helper.py
+++ scripts/helper.py
@@ -5,38 +5,27 @@ import subprocess
 import sys
 import shlex
 
-def parse_input(valid_inputs, input_message="Enter a string: ", output_message="Input not valid",
-                sanitise=True, null_value="", invalid_inputs = []):
-    """Parse and validate input"""
-    # Continue accepting input until valid
-    while True:
-        user_input = input(input_message)
-        if (sanitise):
-            user_input = user_input.replace("/", "")
-            user_input = user_input.replace("\\", "")
-        if user_input == "" and null_value in valid_inputs and null_value not in invalid_inputs:
-            logging.debug("Input set to: {}".format(user_input))
-            return null_value
-        elif user_input not in valid_inputs or user_input in invalid_inputs:
-            print(output_message)
-        else:
-            logging.debug("Input set to: {}".format(user_input))
-            return user_input
+def set_verbosity_level(level):
+    """
+    Sets the verbosity level
+    Parameters:
+        level: The level to set the verbosity at
+    """
+    levels = {
+        "debug": logging.DEBUG,
+        "info": logging.INFO,
+        "warning": logging.WARNING,
+        "error": logging.ERROR,
+        "critical": logging.CRITICAL
+    }
 
-def run_buildit(collector, tests = "", ci=False):
-    host = "localhost" if ci else "ox.moma"
-    base_command = "bin/buildit {} -j /usr/lib/jvm/default-java {}".format(host, collector)
-    test_flag = "" if tests == "" else " -t \"{}\"".format(tests)
-    output = str(subprocess.check_output(shlex.split(base_command + test_flag)))
-    output = output.split("\\n")
-    for line in output:
-        if "jikesrvm/results/buildit" in line:
-            for word in line.split():
-                if "jikesrvm/results/buildit" in word:
-                    directory = word
-                    break
-    results_dir = directory.split("/")[-1]
-    return results_dir
+    log_level = levels.get(level.lower(), -1)
+
+    if log_level != -1:
+        logging.basicConfig(stream=sys.stderr, level=log_level)
+    else:
+        print("Logging level must be a value from", levels.keys())
+        exit(1)
 
 def write_result(file, test_name, result, build):
     """Write data to file"""
@@ -58,26 +47,15 @@ def append_equals(text, length):
     equals_length = length - len(text)
     return "=" * math.floor(equals_length / 2) + text + "=" * math.ceil(equals_length / 2)
 
-def buildit(cmd, env=None, check_corrupted=False):
-    print ("{}".format(cmd))
-    p = subprocess.Popen(cmd,
-                         env=env,
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
-    stdout = []
-    if check_corrupted:
-        while True:
-            line = p.stdout.readline().decode("utf-8")
-            stdout.append(line)
-            sys.stdout.write(line)
-            if ("corrupted zip file") in line:
-                return 3
-            if line == '' and p.poll() != None:
-                break
-    p.wait()
-    return p.returncode
-
 def run(cmd, env=None):
+    """
+    Runs a command
+    Parameters:
+        cmd: The command to execute. Must already be split by shlex
+        env: The environment to run it in
+    Returns:
+        A return code of the command and the stdout of the command
+    """
     print("{}".format(cmd))
     p = subprocess.Popen(cmd,
                          env=env,
@@ -93,11 +71,10 @@ def run(cmd, env=None):
     p.wait()
     return p.returncode, stdout
 
-def build_jvm(collector, host ="localhost", build_args = "", test_run = "",
-              test_suite="", java_home="/usr/lib/jvm/default-java", rerun_corrupted=3, require_results_dir=False):
+def buildit(collector, host ="localhost", build_args = "", test_run = "",
+              test_suite="", java_home="/usr/lib/jvm/default-java", rerun_corrupted=3):
     """
-    Builds JVM with particular collector
-
+    Runs buildit with particular collector
     Args:
         host: Which host to use
         build_args: The build arguments to use
@@ -122,17 +99,15 @@ def build_jvm(collector, host ="localhost", build_args = "", test_run = "",
     if exit_code == 0:
         build = True
     directory = ""
-    if require_results_dir and build:
-        for line in stdout:
-            if "jikesrvm/results/buildit" in line:
-                for word in line.split():
-                    if "jikesrvm/results/buildit" in word:
-                        directory = word
-                        print("found word",word)
-                        break
-    print(directory)
-    results_dir = directory.split("/")[-1]
-    return build, results_dir
+    for line in stdout:
+        if "jikesrvm/results/buildit" in line:
+            for word in line.split():
+                if "jikesrvm/results/buildit" in word:
+                    directory = word
+                    break
+    results_dir = directory.split("/")
+    buildit_index = results_dir.index("buildit")
+    return build, results_dir[buildit_index + 1]
 
 def test_jvm(args):
     """
