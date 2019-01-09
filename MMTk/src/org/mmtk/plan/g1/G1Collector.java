b3c0bfd753793eb65affabbfa16d567dcc6093fb
Wenyu Zhao <wenyuzhaox@gmail.com>
2019-01-09 13:51:13 +1100

    java part of `rust-g1`
    Former-commit-id: c22f8a98f413e5a0d86347fe5a5dc508f345cced

---
+++ MMTk/src/org/mmtk/plan/g1/G1Collector.java
@@ -0,0 +1,177 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+package org.mmtk.plan.g1;
+
+import org.mmtk.plan.*;
+import org.mmtk.policy.CopyLocal;
+import org.mmtk.policy.LargeObjectLocal;
+import org.mmtk.policy.Space;
+import org.mmtk.utility.ForwardingWord;
+import org.mmtk.vm.VM;
+
+import org.vmmagic.unboxed.*;
+import org.vmmagic.pragma.*;
+
+/**
+ * This class implements <i>per-collector thread</i> behavior
+ * and state for the <i>SS</i> plan, which implements a full-heap
+ * semi-space collector.<p>
+ *
+ * Specifically, this class defines <i>SS</i> collection behavior
+ * (through <code>trace</code> and the <code>collectionPhase</code>
+ * method), and collection-time allocation (copying of objects).<p>
+ *
+ * See {@link G1} for an overview of the semi-space algorithm.
+ *
+ * @see G1
+ * @see G1Mutator
+ * @see StopTheWorldCollector
+ * @see CollectorContext
+ */
+@Uninterruptible
+public class G1Collector extends StopTheWorldCollector {
+
+  /****************************************************************************
+   * Instance fields
+   */
+
+  /**
+   *
+   */
+  protected final G1TraceLocal trace;
+  protected final CopyLocal ss;
+  protected final LargeObjectLocal los;
+
+  /****************************************************************************
+   *
+   * Initialization
+   */
+
+  /**
+   * Constructor
+   */
+  public G1Collector() {
+    this(new G1TraceLocal(global().trace));
+  }
+
+  /**
+   * Constructor
+   * @param tr The trace to use
+   */
+  protected G1Collector(G1TraceLocal tr) {
+    ss = new CopyLocal();
+    los = new LargeObjectLocal(Plan.loSpace);
+    trace = tr;
+  }
+
+  /****************************************************************************
+   *
+   * Collection-time allocation
+   */
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  @Inline
+  public Address allocCopy(ObjectReference original, int bytes,
+      int align, int offset, int allocator) {
+    if (allocator == Plan.ALLOC_LOS) {
+      if (VM.VERIFY_ASSERTIONS) VM.assertions._assert(bytes > Plan.MAX_NON_LOS_COPY_BYTES);
+      return los.alloc(bytes, align, offset);
+    } else {
+      if (VM.VERIFY_ASSERTIONS) {
+        VM.assertions._assert(bytes <= Plan.MAX_NON_LOS_COPY_BYTES);
+        VM.assertions._assert(allocator == G1.ALLOC_SS);
+      }
+      return ss.alloc(bytes, align, offset);
+    }
+  }
+
+  @Override
+  @Inline
+  public void postCopy(ObjectReference object, ObjectReference typeRef,
+      int bytes, int allocator) {
+    ForwardingWord.clearForwardingBits(object);
+    if (allocator == Plan.ALLOC_LOS)
+      Plan.loSpace.initializeHeader(object, false);
+  }
+
+  /****************************************************************************
+   *
+   * Collection
+   */
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  @Inline
+  public void collectionPhase(short phaseId, boolean primary) {
+    if (phaseId == G1.PREPARE) {
+      // rebind the copy bump pointer to the appropriate semispace.
+      ss.rebind(G1.toSpace());
+      los.prepare(true);
+      super.collectionPhase(phaseId, primary);
+      return;
+    }
+
+    if (phaseId == G1.CLOSURE) {
+      trace.completeTrace();
+      return;
+    }
+
+    if (phaseId == G1.RELEASE) {
+      trace.release();
+      los.release(true);
+      super.collectionPhase(phaseId, primary);
+      return;
+    }
+
+    super.collectionPhase(phaseId, primary);
+  }
+
+
+  /****************************************************************************
+   *
+   * Object processing and tracing
+   */
+
+  /**
+   * Return {@code true} if the given reference is to an object that is within
+   * one of the semi-spaces.
+   *
+   * @param object The object in question
+   * @return {@code true} if the given reference is to an object that is within
+   * one of the semi-spaces.
+   */
+  public static boolean isSemiSpaceObject(ObjectReference object) {
+    return Space.isInSpace(G1.SS0, object) || Space.isInSpace(G1.SS1, object);
+  }
+
+  /****************************************************************************
+   *
+   * Miscellaneous
+   */
+
+  /** @return The active global plan as an <code>SS</code> instance. */
+  @Inline
+  private static G1 global() {
+    return (G1) VM.activePlan.global();
+  }
+
+  @Override
+  public TraceLocal getCurrentTrace() {
+    return trace;
+  }
+}
---
+++ rvm/src/org/jikesrvm/mm/mminterface/G1Context.java
@@ -0,0 +1,119 @@
+/*
+ *  This file is part of the Jikes RVM project (http://jikesrvm.org).
+ *
+ *  This file is licensed to You under the Eclipse Public License (EPL);
+ *  You may not use this file except in compliance with the License. You
+ *  may obtain a copy of the License at
+ *
+ *      http://www.opensource.org/licenses/eclipse-1.0.php
+ *
+ *  See the COPYRIGHT.txt file distributed with this work for information
+ *  regarding copyright ownership.
+ */
+package org.jikesrvm.mm.mminterface;
+
+import org.jikesrvm.VM;
+import org.mmtk.plan.g1.G1Mutator;
+import org.vmmagic.pragma.*;
+import org.vmmagic.unboxed.*;
+import org.jikesrvm.runtime.Magic;
+
+import static org.jikesrvm.runtime.EntrypointHelper.getField;
+import static org.jikesrvm.runtime.SysCall.sysCall;
+import static org.jikesrvm.runtime.UnboxedSizeConstants.BYTES_IN_WORD;
+
+@Uninterruptible
+public class G1Context extends G1Mutator {
+    @Entrypoint
+    Address threadId;
+    @Entrypoint
+    Address cursor;
+    @Entrypoint
+    Address limit;
+    @Entrypoint
+    Address space;
+    @Entrypoint
+    Address threadIdImmortal;
+    @Entrypoint
+    Address cursorImmortal;
+    @Entrypoint
+    Address limitImmortal;
+    @Entrypoint
+    Address spaceImmortal;
+
+    static final Offset threadIdOffset = getField(SSContext.class, "threadId", Address.class).getOffset();
+    static final Offset cursorOffset = getField(SSContext.class, "cursor", Address.class).getOffset();
+    static final Offset limitOffset = getField(SSContext.class, "limit", Address.class).getOffset();
+    static final Offset spaceOffset = getField(SSContext.class, "space", Address.class).getOffset();
+    static final Offset threadIdImmortalOffset = getField(SSContext.class, "threadIdImmortal", Address.class).getOffset();
+    static final Offset cursorImmortalOffset = getField(SSContext.class, "cursorImmortal", Address.class).getOffset();
+    static final Offset limitImmortalOffset = getField(SSContext.class, "limitImmortal", Address.class).getOffset();
+    static final Offset spaceImmortalOffset = getField(SSContext.class, "spaceImmortal", Address.class).getOffset();
+
+    public Address setBlock(Address mmtkHandle) {
+        if (VM.VerifyAssertions) {
+            VM._assert(cursorOffset.minus(threadIdOffset) == Offset.fromIntSignExtend(BYTES_IN_WORD));
+            VM._assert(limitOffset.minus(threadIdOffset) == Offset.fromIntSignExtend(BYTES_IN_WORD * 2));
+            VM._assert(spaceOffset.minus(threadIdOffset) == Offset.fromIntSignExtend(BYTES_IN_WORD * 3));
+            VM._assert(threadIdImmortalOffset.minus(threadIdOffset) == Offset.fromIntSignExtend(BYTES_IN_WORD * 4));
+            VM._assert(cursorImmortalOffset.minus(threadIdOffset) == Offset.fromIntSignExtend(BYTES_IN_WORD * 5));
+            VM._assert(limitImmortalOffset.minus(threadIdOffset) == Offset.fromIntSignExtend(BYTES_IN_WORD * 6));
+            VM._assert(spaceImmortalOffset.minus(threadIdOffset) == Offset.fromIntSignExtend(BYTES_IN_WORD * 7));
+        }
+        threadId = mmtkHandle.loadAddress();
+        cursor   = mmtkHandle.plus(BYTES_IN_WORD).loadAddress();
+        limit    = mmtkHandle.plus(BYTES_IN_WORD * 2).loadAddress();
+        space    = mmtkHandle.plus(BYTES_IN_WORD * 3).loadAddress();
+        threadIdImmortal = mmtkHandle.plus(BYTES_IN_WORD * 4).loadAddress();
+        cursorImmortal   = mmtkHandle.plus(BYTES_IN_WORD * 5).loadAddress();
+        limitImmortal    = mmtkHandle.plus(BYTES_IN_WORD * 6).loadAddress();
+        spaceImmortal    = mmtkHandle.plus(BYTES_IN_WORD * 7).loadAddress();
+        return Magic.objectAsAddress(this).plus(threadIdOffset);
+    }
+
+    @Override
+    public Address alloc(int bytes, int align, int offset, int allocator, int site) {
+        Address handle = Magic.objectAsAddress(this).plus(threadIdOffset);
+        return sysCall.sysAlloc(handle, bytes, align, offset, allocator);
+        // Address region;
+        // Address cursor;
+        // Address sentinel;
+        // if (allocator == Plan.ALLOC_DEFAULT) {
+        //     cursor = this.cursor;
+        //     sentinel = this.limit;
+        // } else {
+        //     cursor = this.cursorImmortal;
+        //     sentinel = this.limitImmortal;
+        // }
+
+        // // Align allocation
+        // Word mask = Word.fromIntSignExtend(align - 1);
+        // Word negOff = Word.fromIntSignExtend(-offset);
+
+        // Offset delta = negOff.minus(cursor.toWord()).and(mask).toOffset();
+
+        // Address result = cursor.plus(delta);
+
+        // Address newCursor = result.plus(bytes);
+
+        // if (newCursor.GT(sentinel)) {
+        //     Address handle = Magic.objectAsAddress(this).plus(threadIdOffset);
+        //     region = sysCall.sysAllocSlow(handle, bytes, align, offset, allocator);
+        // } else {
+        //     if (allocator == Plan.ALLOC_DEFAULT) {
+        //         this.cursor = newCursor;
+        //     } else {
+        //         this.cursorImmortal = newCursor;
+        //     }
+        //     region = result;
+        // }
+        // return region;
+    }
+
+    @Override
+    public void postAlloc(ObjectReference ref, ObjectReference typeRef,
+                          int bytes, int allocator) {
+        Address handle = Magic.objectAsAddress(this).plus(threadIdOffset);
+        sysCall.sysPostAlloc(handle, ref, typeRef, bytes, allocator);
+    }
+}
